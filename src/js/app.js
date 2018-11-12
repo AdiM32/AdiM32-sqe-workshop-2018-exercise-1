674524a028e6eafcdfe35f0b3449a793b8228229
AdiM32 <adididiadidas@gmail.com>
2018-11-12 22:48:13 +0200

    cases: Program FunctionDeclaration

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,54 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
 
+let model = [];
+
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
-        let model = buildModel(parsedCode);
+        buildModel(parsedCode);
         let table = buildTable(model);
         table;
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        $('#parsedCode').val(JSON.stringify(table, null, 2));
     });
 });
 
 const buildStruct = (...keys) => ((...values) => keys.reduce((obj, key, i) => {obj[key] = values[i]; return obj;} , {}));
 const Row = buildStruct('Line', 'Type', 'Name', 'Condition', 'Value');
 
+function parseFunctionDeclaration(parsedCode) {
+    model.push(Row(parsedCode.loc.start.line, 'function declaration', parsedCode.id.name, '', ''));
+    parseParam(parsedCode.params);
+    model.push(buildModel(parsedCode.body));
+}
+
 function buildModel(parsedCode) {
-    var model = [];
+    // TODO: delete if
+    if (parsedCode === undefined)
+        return;
     switch(parsedCode.type){
-    case 'Program':
-        model = buildModel(parseBody(parsedCode));
-        break;
-    case 'FunctionDeclaration':
-        model.push(parseParam(parsedCode.params));
-        model.push(parseBody(parsedCode.body));
-        model.push(Row(parsedCode.loc.line, 'FunctionDeclaration', parsedCode.id.name, '', ''));
+    case 'Program': buildModel(parseBody(parsedCode.body)); break;
+    case 'FunctionDeclaration': parseFunctionDeclaration(parsedCode); break;
+    case 'BlockStatement': parseBody(parsedCode.body); break;
+    default:
+        //TODO: change
+        model.push(Row('1','2','3','4','5'));
         break;
     // TODO: more cases
     }
-    return model;
 }
 
 function parseBody(body) {
-    return body.reduce((acc, cur) => acc.concat(buildModel(cur)), []);
+    body.forEach((element) => buildModel(element));
 }
 
 function buildTable(model) {
     // TODO: implement
-    var table = model;
+    let table = model;
     return table;
 }
 
 function parseParam(params) {
-    // TODO: implement
-    return [params];
+    params.forEach((param) => model.push(Row(param.loc.start.line, 'variable declaration', param.name, '', '')));
 }
