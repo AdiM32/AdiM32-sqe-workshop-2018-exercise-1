f4ed3bdd9f6308c4c2fb7a17e64e5f086240a3ce
AdiM32 <adididiadidas@gmail.com>
2018-11-13 22:22:43 +0200

    add: VariableDeclaration ExpressionStatement AssignmentExpression: not finished need to implement parseBinaryExpression WhileStatement

--- src/js/app.js
+++ src/js/app.js
@@ -28,27 +28,55 @@ function buildModel(parsedCode) {
     if (parsedCode === undefined)
         return;
     switch(parsedCode.type){
-    case 'Program': buildModel(parseBody(parsedCode.body)); break;
+    case 'Program': parseBody(parsedCode.body); break;
     case 'FunctionDeclaration': parseFunctionDeclaration(parsedCode); break;
     case 'BlockStatement': parseBody(parsedCode.body); break;
+    case 'VariableDeclaration': parseVariableDeclaration(parsedCode.declarations); break;
+    case 'ExpressionStatement': buildModel(parsedCode.expression); break;
+    case 'AssignmentExpression':
+        parseAssignmentExpression(parsedCode.left, parsedCode.right, parsedCode.loc.start.line); break;
+    case 'WhileStatement': parseWhileStatement(parsedCode.test, parsedCode.body, parsedCode.loc.start.line); break;
     default:
-        //TODO: change
+        console.log(parsedCode.type);
         model.push(Row('1','2','3','4','5'));
         break;
-    // TODO: more cases
     }
+}
+
+function parseWhileStatement(test, body, line) {
+    model.push(Row(line, 'while statement', '', test.left.name + test.operator + test.right.name, ''));
+    buildModel(body);
+}
+
+function parseAssignmentExpression(left, right, line) {
+    model.push(Row(line, 'assignment expression', left.name, '', find_init(right)));
+}
+
+function find_init(init) {
+    if (init.type === 'Literal')
+        return init.raw;
+    if (init.type === 'BinaryExpression')
+        // TODO: parse BinaryExpression
+        return init.left.name + init.operator + init.right.raw;
+}
+
+function parseVariableDeclaration(declarations) {
+    declarations.forEach((element) => {
+        model.push(Row(element.loc.start.line, 'variable declaration', element.id.name, '' ,
+            element.init === null? 'null (or nothing)': element.init));
+    });
 }
 
 function parseBody(body) {
     body.forEach((element) => buildModel(element));
 }
 
+function parseParam(params) {
+    params.forEach((param) => model.push(Row(param.loc.start.line, 'variable declaration', param.name, '', '')));
+}
+
 function buildTable(model) {
     // TODO: implement
     let table = model;
     return table;
-}
-
-function parseParam(params) {
-    params.forEach((param) => model.push(Row(param.loc.start.line, 'variable declaration', param.name, '', '')));
 }
